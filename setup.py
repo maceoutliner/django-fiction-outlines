18e3cf4e6e513416a61c763db5f33ebc11cab296
Daniel Andrlik <daniel@andrlik.org>
2018-04-07 22:16:49 -0400

    Bump version number.

--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
 )
