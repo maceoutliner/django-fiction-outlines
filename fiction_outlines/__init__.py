1040d6dc5df5d09b2b7ea8c8aa247e8a72a56225
Daniel Andrlik <daniel@andrlik.org>
2022-03-17 13:52:04 -0400

    WIP conversion of build system for upcoming release.

--- fiction_outlines/__init__.py
+++ fiction_outlines/__init__.py
@@ -2,5 +2,4 @@
 fiction_outlines is a reusable Django app for managing manuscript outlines.
 '''
 
-__version__ = '0.3.1'
-default_app_config = 'fiction_outlines.apps.FictionOutlinesConfig'
+__version__ = '0.4.0'
